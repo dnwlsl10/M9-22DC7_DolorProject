1cc4069ec46ae12696620e56dc3e66505f8d8f52
jpointw <99316239+jpointw@users.noreply.github.com>
2022-06-21 14:18:26 +0900

    UI 0.0.4

--- Assets/2.Script/JW/UIManager.cs
+++ Assets/2.Script/JW/UIManager.cs
@@ -19,39 +19,57 @@ public class UIManager : MonoBehaviour
     }
 
     [Header("HP_STATUS")]
-    [SerializeField] private Slider HP;
-    [SerializeField] private Text HP_text;
-    //[Header("SKILL_STATUS")]
-    //[SerializeField] private Slider Sheild;
-    //[SerializeField] private Slider TeslaCanon;
-    //[SerializeField] private Slider Ultimate_Skill;
+        [SerializeField] private Slider HP;
+        [SerializeField] private Text HP_text;
+    [Header("SKILL_STATUS")]
+        //[SerializeField] private Slider Sheild;
+        //[SerializeField] private Slider TeslaCanon;
+        [SerializeField] private Slider Ultimate_Skill;
+        [SerializeField] private Slider Shield_Skill;
+        [SerializeField] private Slider Tesla_Skill;
 
-    public void UpdateHPGauge(float damageAmount)
-    {
-        HP.value -= damageAmount / 100f;
-        //HP_text.text = "" + ;
-    }
+    float ultimateChargingTime = 0.0001f;
+    float normalChargeingTime = 0.001f;
+
+    // public void UpdateHPGauge(float damageAmount)
+    // {
+    //     HP.value -= damageAmount / 100f;
+    //     //HP_text.text = "" + ;
+    // }
     void HPUpdate(float cur_val, float max_val)
     {
         float per = cur_val / max_val;
+        HP.value = per;
+        HP_text.text = "" + (per * 100);
     }
-
-
-    //public void UpdateSheidGauge(int shield_guage)
-    //{
-
-    //}
-    //public void UpdateTeslaGauge(int tesla_gauge)
-    //{
-
-    //}
-    //public void UpdateUltimateSkillGauge(int US_gauge)
-    //{
-
-    //}
-    private void OnEnable()
+    public void UpdateUltimateSkillGauge()
     {
-        BasicWeapon bw = null;
-        bw.OnValueChange += HPUpdate;
+        if(Ultimate_Skill.value >= 1) return;
+        else Ultimate_Skill.value += ultimateChargingTime; 
     }
+
+    public void UpdateTeslaSkillGauge()
+    {
+        if(Tesla_Skill.value >= 1) return;
+        else Tesla_Skill.value += normalChargeingTime; 
+    }
+
+    public void UpdateSheildSkillGauge()
+    {
+        if(Shield_Skill.value >= 1) return;
+        else Shield_Skill.value += normalChargeingTime; 
+    }
+
+    private void FixedUpdate() 
+    {
+           UpdateUltimateSkillGauge();
+           UpdateTeslaSkillGauge();
+           UpdateSheildSkillGauge();
+    }
+
+    // private void OnEnable()
+    // {
+    //     BasicWeapon bw = null;
+    //     bw.OnValueChange += HPUpdate;
+    // }
 }
