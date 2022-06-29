d16c7469817242f6664e6697f1708c4c4d38ea5b
jpointw <99316239+jpointw@users.noreply.github.com>
2022-06-29 14:13:14 +0900

    Map Upload

---
+++ Assets/0.Assets/Beautify/URP/Demo/DemoSources/Scripts/SphereAnimator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+
+namespace Beautify.Universal {
+
+    public class SphereAnimator : MonoBehaviour {
+
+        Rigidbody rb;
+
+        void Start() {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        void FixedUpdate() {
+            if (transform.position.z < 1f) {
+                rb.AddForce(Vector3.forward * 10f);
+            } else if (transform.position.z > 8f) {
+                rb.AddForce(-Vector3.forward * 10f);
+            }
+        }
+    }
+}
