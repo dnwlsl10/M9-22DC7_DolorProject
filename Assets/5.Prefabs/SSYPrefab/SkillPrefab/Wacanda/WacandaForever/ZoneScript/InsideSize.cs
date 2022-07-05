c3a323dd3543ae28b74d3129f3194abeea8d7e05
Soo Young <offlineserviceseoul@gmail.com>
2022-07-05 14:41:35 +0900

    맵 1차 자기장 완료

---
+++ Assets/5.Prefabs/SSYPrefab/SkillPrefab/Wacanda/WacandaForever/ZoneScript/InsideSize.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InsideSize : MonoBehaviour
+{
+    MeshRenderer mr;
+   [SerializeField] Material mat;
+
+    [Header("change value")]
+    public float changeVal;
+
+    [Header("defult value")]
+    [SerializeField] float dissolveMinValue = 1.2f;
+    [SerializeField] float dissolveMaxValue = -0.2f;
+    void Start()
+    {
+        transform.GetChild(0).gameObject.SetActive(false);
+        mr = transform.GetChild(1).GetComponent<MeshRenderer>();
+        mat = mr.material;
+        changeVal = dissolveMinValue;
+        mat.SetFloat("_Dissolve", changeVal);
+        StartCoroutine(ChangeInsideSize());
+    }
+
+    IEnumerator ChangeInsideSize()
+    {
+        while(changeVal > dissolveMaxValue)
+        {
+            changeVal -= 0.001f;
+            mat.SetFloat("_Dissolve", changeVal);
+            yield return new WaitForEndOfFrame();
+        }
+            transform.GetChild(0).gameObject.SetActive(true);
+        StartCoroutine(ChangeInsideSizeSecond());
+    }
+    IEnumerator ChangeInsideSizeSecond()
+    {
+        while (changeVal < dissolveMinValue) //-0.2에서 1.2f가 될 때까지 반복
+        {
+            changeVal += 0.001f;
+            mat.SetFloat("_Dissolve", changeVal);
+            yield return new WaitForEndOfFrame();
+        }
+            transform.GetChild(1).gameObject.SetActive(false);
+    }
+}
