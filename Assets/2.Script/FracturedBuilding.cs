4e4a2da62882f37dfef48a173291c14523267287
jpointw <99316239+jpointw@users.noreply.github.com>
2022-07-12 20:18:32 +0900

    map real final

---
+++ Assets/2.Script/FracturedBuilding.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FracturedBuilding : MonoBehaviour
+{
+    Rigidbody[] rbs;
+    private void Awake() {
+        rbs = GetComponentsInChildren<Rigidbody>();
+    }
+    private void OnDisable() {
+        for (int i = 0; i < rbs.Length; i++)
+        {
+            rbs[i].velocity = rbs[i].angularVelocity = Vector3.zero;
+            rbs[i].transform.localPosition = rbs[i].transform.localEulerAngles = Vector3.zero;
+        }
+    }
+}
