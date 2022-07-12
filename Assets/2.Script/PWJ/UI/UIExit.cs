cc540d46e2e1760b0fb82a4610b90d6eca93fb8a
Soo Young <offlineserviceseoul@gmail.com>
2022-07-12 15:02:26 +0900

    ;;

--- Assets/2.Script/PWJ/UI/UIExit.cs
+++ Assets/2.Script/PWJ/UI/UIExit.cs
@@ -7,11 +7,9 @@ public class UIExit : MonoBehaviour
     public System.Action OnSelceted;
     public AudioClip onTouchSFX;
     private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.layer == 10)
-        {   
+    { 
             AudioPool.instance.Play(onTouchSFX.name, 2, this.transform.position);
             OnSelceted();
-        }
+       
     }
 }
--- Assets/2.Script/PWJ/UI/UIPracice.cs
+++ Assets/2.Script/PWJ/UI/UIPracice.cs
@@ -8,10 +8,9 @@ public class UIPracice : MonoBehaviour
     public AudioClip onTouchSFX;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.layer == 10)
-        {
+
             AudioPool.instance.Play(onTouchSFX.name, 2, this.transform.position);
             OnSelceted();
-        }
+        
     }
 }
